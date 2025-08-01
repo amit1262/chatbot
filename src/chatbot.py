01ef2d05fce49b8aae56c4da03bab670d47da835
amit1262 <amit.sharma1262@gmail.com>
2025-08-01 16:51:13 +0800

    Basic version of Chatbot: 1. Uses Azure OpenAI models 2. Uses System prompt templates to setup a role and instructions for the model 3. Uses LangGraph for memory management and a custom workflow for execution.

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv
 import os
 from langchain_ollama import ChatOllama
+from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import (
     PromptTemplate,
     SystemMessagePromptTemplate,
 )
 from langgraph.graph import START, END, StateGraph
-from typing import TypedDict
-from typing_extensions import Annotated
+from typing import TypedDict, Annotated
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
 
@@ -22,33 +22,23 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-def load_dotenv(override: bool = True):
-    # Load environment variables from .env file
-    status = load_dotenv(override=True)
-    assert status, "Failed to load environment variables from .env file"
-
-
-def _get_system_prompts():
-    # Define a system message to set the context for the conversation
-    system_prompt = SystemMessagePromptTemplate.from_template(
-        "You are a professional assistant with deep expertise in {domain}."
-    )
-    # decide a domain for the chatbot
-    chatbot_domain = input(
-        "Enter the domain for the chatbot (e.g., medical, sports, science): "
-    ).strip()
-    system_message = system_prompt.format(domain=chatbot_domain)
-    assert system_message, "Failed to create system message"
-    return system_message
-
-
 def get_initial_state() -> State:
     """
     Initialize the state of the chatbot.
     This can be extended to include more fields as needed.
     """
-    system_prompts = _get_system_prompts()
-    return {"messages": [system_prompts]}
+    # Define a system message to set the context for the conversation
+    system_prompt = SystemMessagePromptTemplate.from_template(
+        "You are an experienced clinical researcher with lots of knowledge for conducting network studies, in OHDSI ecosystem, using R and Strategus packages."
+        "Today, you will be helping a user with generating a complete and executable R code for a Strategus network study."
+        "let's start by asking the user about what kind of network study they want to conduct."
+        "Given the user's input, first estimate if question is complete, if not, ask the user for more details."
+        "Your job is to guide the user step by step - for each step, ask the user for specific details related to that step and then generate the R code based on their input."
+        "At any point, just discuss only the next step and wait for the user to provide the necessary details."
+        "No need to explain the conceptual details about any step - unless asked by the user."
+        "Assume the user doesn't know anything about Strategus or R and guide them through the process - one step at a time."
+    )
+    return {"messages": [system_prompt.format()]}
 
 
 def invoke_model(current_state: State) -> State:
@@ -65,6 +55,9 @@ def invoke_model(current_state: State) -> State:
         token = chunk.text()  # or chunk.text() in some versions
         print(token, end="", flush=True)
         full_response += token
+    print("\n")
+    print("*" * 100)
+    print("\n")
 
     return {"messages": [AIMessage(content=full_response)]}
 
@@ -88,17 +81,17 @@ def initialize_model():
     This function can be extended to include more complex logic.
     """
     # model initialization
-    model = ChatOllama(
-        model="llama3.2:latest", temperature=0.1, max_tokens=1000, streaming=True
-    )
+    MODEL_NAME = "azure_openai:gpt-4o"
+    model = init_chat_model(model=MODEL_NAME)
     assert model, "Failed to initialize ChatOllama model"
-    print("ChatOllama model initialized successfully.")
+    print(f"{MODEL_NAME} initialized successfully.")
     return model
 
 
 if __name__ == "__main__":
-    load_dotenv
+    load_dotenv()
     model = initialize_model()
+    print("*" * 100)
 
     # create a workflow graph
     workflow = StateGraph(state_schema=State)
@@ -108,12 +101,15 @@ if __name__ == "__main__":
 
     # invoke the graph
     memory = MemorySaver()
-    config = {"configurable": {"thread_id": "bot_1"}}
+    config = {"configurable": {"thread_id": "bot_2"}}
     workflow_compiled = workflow.compile(checkpointer=memory)
     first_run = config.get("configurable", {}).get("thread_id") not in memory.storage
     initial_state = get_initial_state()
 
     while True:
+        first_run = (
+            config.get("configurable", {}).get("thread_id") not in memory.storage
+        )
         try:
             user_input = input("\n\nUser: ").strip()
             if user_input.lower() in ["exit", "quit", "q"]:
@@ -122,11 +118,11 @@ if __name__ == "__main__":
             if first_run:
                 initial_state["messages"].append(HumanMessage(content=user_input))
                 state = initial_state
-                print("First run, state:", state)
+                # print("First run, state:", state)
             else:
                 state = {"messages": [HumanMessage(content=user_input)]}
-                print("state:", state)
+                # print("state:", state)
             workflow_compiled.invoke(state, config=config)
-        except:
-            print("An error occurred. Please try again.")
+        except Exception as e:
+            print(f"An error occurred. {e}")
             break
